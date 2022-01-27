98c70a9dc0aa1426fef9cbf3cbbbec3cc4dd645d
ALEXANDRE DA SILVA PALMA <63171818+alexandrecrudi@users.noreply.github.com>
2022-01-27 18:40:34 -0300

    Add files via upload

---
+++ DesignResponsivo1/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Página de Teste</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="topo">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="corpo"></div>
+        <div class="rodape"></div>
+    </body>
+</html>
